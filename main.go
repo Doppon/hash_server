939bd787fad25a5d15d7167dd19eef650182cc7c
Doppon <zaxpkuraudo7777@gmail.com>
2020-02-02 17:30:19 +0900

    httpサーバーでhellowordするプログラムを追加

---
+++ main.go
@@ -0,0 +1,20 @@
+// main.go
+
+package main
+
+import (
+    "io"
+    "net/http"
+    // "os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "Hello World!")
+}
+
+func main() {
+    // port := os.Getenv("PORT")
+    http.HandleFunc("/", hello)
+    // http.ListenAndServe(":"+port, nil)
+    http.ListenAndServe(":8080", nil)
+}
